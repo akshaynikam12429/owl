deb03825487be7ab39031eee7ccf20f0cc5a1fd5
amruth33 <37182034+amruth33@users.noreply.github.com>
2020-10-29 09:52:45 +0530

    Update email.c

--- esb_proj/esb_app/src/adapter/email.c
+++ esb_proj/esb_app/src/adapter/email.c
@@ -2,4 +2,16 @@
 
 void send(char *to, char *text) {
     printf("Sending to %s\n", to);
+    /*char cmd[100];  // to hold the command.
+      
+        char tempFile[100];     // name of tempfile.
+
+        strcpy(tempFile,tempnam("/tmp","sendmail")); // generate temp file name.
+
+        FILE *fp = fopen(tempFile,"w"); // open it for writing.
+        fprintf(fp,"%s\n",text);        // write body to it.
+        fclose(fp);             // close it.
+
+        sprintf(cmd,"ssmtp %s < %s",to,tempFile); // prepare command.
+        system(cmd);     // execute it.*/
 }
