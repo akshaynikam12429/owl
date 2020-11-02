f8f97b76da04f85b93542b0829d1337997c4707c
Amruth <amruth@amruth-ubuntu>
2020-11-02 18:02:58 +0530

    updated esb_app

--- esb_proj/esb_app/src/esb_app.c
+++ esb_proj/esb_app/src/esb_app.c
@@ -28,7 +28,7 @@
  *
  * Note that the above is probably not what you want to do in real life.
  */
-#include <stdbool.h>
+
 #include <kore/kore.h>
 #include <kore/http.h>
 #include <pthread.h>
@@ -36,188 +36,44 @@
 #include <unistd.h>
 #include <sys/time.h>
 #include "esb.h"
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <sys/un.h>
-#include <stddef.h>
-
+#include "esb/client.h"
 #define PATH_MAX 500
 
-void log_msg(const char *msg, bool terminate) {
-    printf("%s\n", msg);
-    if (terminate) exit(-1); /* failure */
-}
-
-int make_named_socket(const char *socket_file, bool is_client) {
-    printf("Creating AF_LOCAL socket at path %s\n", socket_file);
-    if (!is_client && access(socket_file, F_OK) != -1) {
-        log_msg("An old socket file exists, removing it.", false);
-        if (unlink(socket_file) != 0) {
-            log_msg("Failed to remove the existing socket file.", true);
-        }
-    }
-    struct sockaddr_un name;
-    /* Create the socket. */
-    int sock_fd = socket(AF_LOCAL, SOCK_STREAM, 0);
-    if (sock_fd < 0) {
-        log_msg("Failed to create socket.", true);
-    }
-
-    /* Bind a name to the socket. */
-    name.sun_family = AF_LOCAL;
-    strncpy (name.sun_path, socket_file, sizeof(name.sun_path));
-    name.sun_path[sizeof(name.sun_path) - 1] = '\0';
-
-    /* The size of the address is
-       the offset of the start of the socket_file,
-       plus its length (not including the terminating null byte).
-       Alternatively you can just do:
-       size = SUN_LEN (&name);
-   */
-    size_t size = (offsetof(struct sockaddr_un, sun_path) +
-                   strlen(name.sun_path));
-    if (is_client) {
-        if (connect(sock_fd, (struct sockaddr *) &name, size) < 0) {
-            log_msg("connect failed", 1);
-        }
-    } else {
-        if (bind(sock_fd, (struct sockaddr *) &name, size) < 0) {
-            log_msg("bind failed", 1);
-        }
-    }
-    return sock_fd;
-}
-
-_Noreturn void start_server_socket(char *socket_file, int max_connects) {
-    int sock_fd = make_named_socket(socket_file, false);
-
-    /* listen for clients, up to MaxConnects */
-    if (listen(sock_fd, max_connects) < 0) {
-        log_msg("Listen call on the socket failed. Terminating.", true); /* terminate */
-    }
-    log_msg("Listening for client connections...\n", false);
-    /* Listens indefinitely */
-    while (1) {
-        struct sockaddr_in caddr; /* client address */
-        int len = sizeof(caddr);  /* address length could change */
-
-        printf("Waiting for incoming connections...\n");
-        int client_fd = accept(sock_fd, (struct sockaddr *) &caddr, &len);  /* accept blocks */
-
-        if (client_fd < 0) {
-            log_msg("accept() failed. Continuing to next.", 0); /* don't terminate, though there's a problem */
-            continue;
-        }
-        /* Start a worker thread to handle the received connection. */
-        /*if (!create_worker_thread(client_fd)) {
-            log_msg("Failed to create worker thread. Continuing to next.", 0);
-            continue;
-        }*/
-
-    }  /* while(1) */
-}
-
-
-void thread_function(int sock_fd) {
-    log_msg("SERVER: thread_function: starting", false);
-    char buffer[5000];
-    memset(buffer, '\0', sizeof(buffer));
-    int count = read(sock_fd, buffer, sizeof(buffer));
-    if (count > 0) {
-        printf("SERVER: Received from client: %s\n", buffer);
-        write(sock_fd, buffer, sizeof(buffer)); /* echo as confirmation */
-    }
-    close(sock_fd); /* break connection */
-    log_msg("SERVER: thread_function: Done. Worker thread terminating.", false);
-    pthread_exit(NULL); // Must be the last statement
-}
-
-/**
- * This function launches a new worker thread.
- * @param sock_fd
- * @return Return true if thread is successfully created, otherwise false.
- */
-bool create_worker_thread(int sock_fd) {
-    log_msg("SERVER: Creating a worker thread.", false);
-    pthread_t thr_id;
-    int rc = pthread_create(&thr_id,
-            /* Attributes of the new thread, if any. */
-                            NULL,
-            /* Pointer to the function which will be
-             * executed in new thread. */
-                            thread_function,
-            /* Argument to be passed to the above
-             * thread function. */
-                            (void *) sock_fd);
-    if (rc) {
-        log_msg("SERVER: Failed to create thread.", false);
-        return false;
-    }
-    return true;
-}
-
-
-int send_bmd_path_to_socket(char *msg, char *socket_file){
-	/**
-	 * TODO: This function can be implemented in a separate module
-	 * which will also implement the socket server.
-	 */
-	int sockfd = make_named_socket(socket_file, true);
-
-    /* Write some stuff and read the echoes. */
-    log_msg("CLIENT: Connect to server, about to write some stuff...", false);
-    if (write(sockfd, msg, strlen(msg)) > 0) {
-        /* get confirmation echoed from server and print */
-        char buffer[5000];
-        memset(buffer, '\0', sizeof(buffer));
-        if (read(sockfd, buffer, sizeof(buffer)) > 0) {
-            printf("CLIENT: Received from server:: %s\n", buffer);
-        }
-    }
-	//int esb_s = process_esb_request(msg);
-    log_msg("CLIENT: Processing done, about to exit...", false);
-    close(sockfd); /* close the connection */
-	//return esb_s; /* 1 => Success */
-}
 
 /**
  * Define a suitable struct for holding the endpoint request handling result.
  */
+
 typedef struct
 {
 	int status;
 	char *bmd_path;
 } endpoint_result;
 
-const char* my_sock_file = "./my_sock";
-
+const char* my_sock_file = "./src/esb/my_sock";
 
 int esb_endpoint(struct http_request *);
 endpoint_result save_bmd(struct http_request *);
 
 /**
- * HTTP request handler function mapped in the conf file. 
+ * HTTP request handler function mapped in the conf file.
  */
-
-int esb_endpoint(struct http_request *req) 
-{	
-	//start_server_socket(my_sock_file,10);
+int esb_endpoint(struct http_request *req)
+{
 	printf("Received the BMD request.\n");
 	endpoint_result epr = save_bmd(req);
-	if (epr.status < 0) 
+	if (epr.status < 0)
 	{
 		printf("Failed to handle the BMD request.\n");
 		return (KORE_RESULT_ERROR);
 	}
 	else
-	{	
-		
+	{printf("file path is %s\n",epr.bmd_path);
+	         //int status=process_esb_request(epr.bmd_path);
 		int sock_status = send_bmd_path_to_socket(epr.bmd_path, my_sock_file);
 		if (sock_status > 0)
 		{
+		
 			kore_log(LOG_INFO, "BMD path sent to server socket.");
 			return (KORE_RESULT_OK);
 		}
@@ -326,7 +182,6 @@ cleanup:
 endpoint_result
 save_bmd(struct http_request *req)
 {
-	start_server_socket(my_sock_file,10);
 	endpoint_result ep_res;
 	/* Default to OK. 1 => OK, -ve => Errors */
 	ep_res.status = 1;
