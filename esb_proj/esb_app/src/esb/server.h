3676b29121d15ea9ea6578701fb4996d61cadd37
Amruth <amruth@amruth-ubuntu>
2020-11-04 08:45:39 +0530

    updated..

--- esb_proj/esb_app/src/esb/server.h
+++ esb_proj/esb_app/src/esb/server.h
@@ -2,7 +2,9 @@
 #define SERVER_H
 
 #include "esb.h"
+#include<stdbool.h>
 
+extern int process_esb_request(char* bmd_file_path);
 int make_named_socket(const char *socket_file, bool is_client);
 void log_msg(const char *msg, bool terminate);
 
