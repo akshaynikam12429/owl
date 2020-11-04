e3edb38a0d2bd9f317b2884d8e89aa1d4262ded3
Amruth <amruth@amruth-ubuntu>
2020-11-04 22:55:29 +0530

    changed from

--- esb_proj/esb_app/src/adapter/email.c
+++ esb_proj/esb_app/src/adapter/email.c
@@ -4,24 +4,22 @@
 
 void  emailsender(char *to,  char *text)
 {
-     char cmd[100];  // to hold the command.
+     char cmd[1000];  // to hold the command.
      int len = strlen(text);
         char tempFile[len];     // name of tempfile.
 
-        strcpy(tempFile,tempnam("/tmp","sendmail")); // generate temp file name.
+        //strcpy(tempFile,tempnam("/tmp","sendmail")); // generate temp file name.
 
-        FILE *fp = fopen(tempFile,"w"); // open it for writing.
-        
-        fprintf(fp,"Subject:payload\n");  
-        fprintf(fp,"From:TeamOwl\r\n");      // write body to it.
-        //fclose(fp);
-
-        //FILE *fp = fopen(tempFile,"w"); // open it for writing.
+        FILE *fp = fopen(tmpfile,"w"); // open it for writing.
+        fprintf(fp,"From: Team Owl\r\r\n");
+        fprintf(fp,"Subject:Payload\r\n");  
+         fprintf(fp,"\r\n");  
         fprintf(fp,"%s\n",text);        // write body to it.
         fclose(fp);             // close it.
          printf("received=%s\n",text);
-        sprintf(cmd,"ssmtp %s < %s",to,tempFile); // prepare command.
+        sprintf(cmd,"ssmtp %s < %s",to,tmpfile); // prepare command.
          system(cmd);     // execute it.
         
-
 }
+
+
